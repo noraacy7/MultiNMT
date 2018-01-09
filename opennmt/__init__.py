2f2b6c2bacd54cc4a69906597b34b9765185cbf8
William Jiang <WilliamJiang@gmail.com>
2018-01-09 10:51:10 +0800

    Delete OpenNMT directory

--- opennmt/__init__.py
+++
@@ -1,7 +0,0 @@
-"""OpenNMT module."""
-
-from opennmt import decoders
-from opennmt import encoders
-from opennmt import inputters
-from opennmt import models
-from opennmt import tokenizers
--- opennmt/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Module defining models."""
-
-from opennmt.models.transformer import Transformer
-from opennmt.models.sequence_to_sequence import SequenceToSequence
-from opennmt.models.sequence_tagger import SequenceTagger
-from opennmt.models.sequence_classifier import SequenceClassifier
