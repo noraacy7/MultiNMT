bb2a60c70c3b98e653fb9cdfc6e6ac238c12416c
William Jiang <WilliamJiang@gmail.com>
2018-01-09 15:56:50 +0800

    Delete main

--- nmt/nmt.py
+++ nmt/nmt.py
@@ -596,10 +596,3 @@ def main(unused_argv):
   train_fn = train.train
   inference_fn = inference.inference
   run_main(FLAGS, default_hparams, train_fn, inference_fn)
-
-
-if __name__ == "__main__":
-  nmt_parser = argparse.ArgumentParser()
-  add_arguments(nmt_parser)
-  FLAGS, unparsed = nmt_parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
