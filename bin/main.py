16146b9aec49ca4e3d918352b20f012483583fb0
William Jiang <WilliamJiang@gmail.com>
2018-01-09 15:28:35 +0800

    Change main.py

--- bin/main.py
+++ bin/main.py
@@ -10,5 +10,5 @@ import sys
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     nmt.add_arguments(parser)
-    FLAGS, unparsed = parser.parse_known_args()
+    nmt.FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=nmt.main, argv=[sys.argv[0]] + unparsed)
